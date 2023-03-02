c5b52ea387886690d00b0a7ed8a2869a4cca1a71
karthik6516 <114976324+karthik6516@users.noreply.github.com>
2023-03-02 23:11:46 +0530

    Added solution - LeetHub

--- Peak element - GFG/peak-element.cpp
+++ Peak element - GFG/peak-element.cpp
@@ -17,9 +17,10 @@ class Solution
        // Your code here
        if(arr[0]>=arr[1])  return 0;
        if(arr[n-1]>=arr[n-2])  return n-1;
+       
        for(int i=1;i<n-1;i++)
        {
-           if(arr[i]>=arr[i-1] && arr[i]>=arr[i+1])  return i;
+           if(arr[i]>=arr[i-1] && arr[i]>=arr[i+1])     return i;
        }
        return -1;
     }
