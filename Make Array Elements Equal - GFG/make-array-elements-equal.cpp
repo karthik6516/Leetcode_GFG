d74ab45f7dc304437cc92c137cc58651a172dd28
karthik6516 <114976324+karthik6516@users.noreply.github.com>
2023-04-01 19:18:25 +0530

    Added solution - LeetHub

---
+++ Make Array Elements Equal - GFG/make-array-elements-equal.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    long long int minOperations(int N) {
+        // Code here
+        if(N%2==0)
+        {
+            long long mid=N/2;
+            return mid*mid;
+        }
+        else
+        {
+           long long e=N/2; 
+           return e*(e+1);
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.minOperations(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
