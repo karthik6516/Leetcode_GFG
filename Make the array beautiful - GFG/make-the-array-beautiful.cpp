684c45df60464259be285a134dd53b7abb680688
karthik6516 <114976324+karthik6516@users.noreply.github.com>
2023-04-13 15:12:38 +0530

    Added solution - LeetHub

---
+++ Make the array beautiful - GFG/make-the-array-beautiful.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> makeBeautiful(vector<int> arr) {
+        // code here
+        vector<int> v;
+        v.push_back(arr[0]);
+        for(int i=1;i<arr.size();i++)
+        {
+            if(v.size()==0)  v.push_back(arr[i]);
+            else
+            {
+            if((v.back()<0 && arr[i]>=0) || (v.back()>=0 && arr[i]<0))  v.pop_back();
+            else
+            v.push_back(arr[i]);
+            }
+        }
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        Solution obj;
+        vector<int> res = obj.makeBeautiful(arr);
+        for (int i = 0; i < res.size(); i++) {
+            cout << res[i] << " ";
+        }
+
+        cout << "\n";
+    }
+}
+// } Driver Code Ends
