52bc8f238d6c139ec0156f57d18321bf8c055c31
karthik6516 <114976324+karthik6516@users.noreply.github.com>
2023-04-13 23:38:44 +0530

    Time: 2 ms (87.29%), Space: 8.4 MB (72.29%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int> v;
+        for(int i=0;i<operations.size();i++)
+        {
+            if(operations[i]=="C")
+                v.pop_back();
+            else if(operations[i]=="D")
+                v.push_back(2*v.back());
+            else if(operations[i]=="+")
+               v.push_back(v[v.size()-1]+v[v.size()-2]);
+            else
+                v.push_back(stoi(operations[i]));
+        }
+        return accumulate(v.begin(),v.end(),0);
+    }
+};
