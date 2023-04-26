e29678e13cd3089cad412702c9d701ded2ebb58e
karthik6516 <114976324+karthik6516@users.noreply.github.com>
2023-04-26 10:45:52 +0530

    Added solution - LeetHub

--- Seating Arrangement - GFG/seating-arrangement.cpp
+++ Seating Arrangement - GFG/seating-arrangement.cpp
@@ -15,10 +15,7 @@ class Solution{
         int count=0;
         for(int i=0;i<m;i++)
         {
-            bool left=false,right=false;
-            if(i==0 || seats[i-1]==0)  left=true;
-            if(i==m-1 || seats[i+1]==0)  right=true;
-           if(seats[i]==0 && left && right)
+           if(seats[i]==0 && (i==0 || seats[i-1]==0) && (i==m-1 || seats[i+1]==0))
            {
                count++;
                seats[i]=1;
